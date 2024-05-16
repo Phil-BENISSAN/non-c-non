bbbbc5ce320e73fb71d7c061c2efbf2b16fd88f7
JulienChevreauPro <julienchevreau33@gmail.com>
2024-05-17 00:57:40 +0200

    made the foods component jsx and css

--- client/src/components/Foods.jsx
+++ client/src/components/Foods.jsx
@@ -1,8 +1,59 @@
+import PropTypes from "prop-types";
 
-function Foods() {
+import "./Foods.css";
+
+function Foods({ food }) {
+  const {
+    type,
+    nom,
+    photo_legume,
+    Exposition,
+    Rusticité,
+    info_pratique,
+    difficulté,
+    Profondeur_semis,
+    Espacement,
+    Ecart_rangs,
+    temps_levé,
+    température_levée,
+    semis_récolte,
+  } = food;
+
   return (
-    <div>Foods</div>
-  )
+    <article className="food_card">
+      <h2 className="food_name"> {nom} </h2>
+      <h3 className="food_type"> {type} </h3>
+      <img src={photo_legume} alt={nom} className="food_photo" />
+      <p>Exposition : {Exposition}. </p>
+      <p>Rusticité : {Rusticité}. </p>
+      <p>À savoir : {info_pratique}. </p>
+      <p>Difficulté : {difficulté} </p>
+      <p>Profondeur des semis : {Profondeur_semis} cm.</p>
+      <p>Espacement : {Espacement} cm.</p>
+      <p>Écart entre les rangs : {Ecart_rangs} cm.</p>
+      <p>Temps de levée : {temps_levé} jours.</p>
+      <p>Température de levée : {température_levée}°c. </p>
+      <p>Du semis à la récolte : {semis_récolte} jours.</p>
+    </article>
+  );
 }
+
+Foods.propTypes = {
+  food: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    nom: PropTypes.string.isRequired,
+    photo_legume: PropTypes.string.isRequired,
+    Exposition: PropTypes.string.isRequired,
+    Rusticité: PropTypes.string.isRequired,
+    info_pratique: PropTypes.string.isRequired,
+    difficulté: PropTypes.number.isRequired,
+    Profondeur_semis: PropTypes.number.isRequired,
+    Espacement: PropTypes.number.isRequired,
+    Ecart_rangs: PropTypes.number.isRequired,
+    temps_levé: PropTypes.number.isRequired,
+    température_levée: PropTypes.number.isRequired,
+    semis_récolte: PropTypes.string.isRequired,
+  }),
+};
 
 export default Foods;
