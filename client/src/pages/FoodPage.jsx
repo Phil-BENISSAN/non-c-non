66e6fc40c2d77c2e75a9a3b78b19724977a8bd4e
JulienChevreauPro <julienchevreau33@gmail.com>
2024-05-16 23:28:06 +0200

    created filter

--- client/src/pages/FoodPage.jsx
+++ client/src/pages/FoodPage.jsx
@@ -1,8 +1,64 @@
+import { useEffect, useState } from "react";
+import { useLoaderData, Link } from "react-router-dom";
+
+import Foods from "../components/Foods";
 
 function FoodPage() {
-  return (
-    <h1>FoodPage</h1>
-  )
-}
+    const foodList = useLoaderData;
+    const [foods, setFoods] = useState([]);
+      
+        useEffect(() => {
+          fetch("http://localhost:3310/api/foods")
+            .then((response) => response.json())
+            .then((data) => setFoods(data))
+            .catch((err) => console.error(err));
+        }, []);
+      
+        const [selectedFood, setSelectedFood] = useState("");
+      
+        const handleFoodChange = (event) => {
+          setSelectedFood(event.target.value);
+        };
+        const filteredFood =
+          selectedFood === ""
+            ? foodList
+            : foodList.filter(
+                (food) => food.type === selectedFood
+              );
+      
+        return (
+          <>
+            <h1>My foods</h1>
+            <form className="food_form">
+              <label htmlFor="food_select">
+                Filter by type {" "}
+                <select
+                  id="food_select"
+                  onChange={handleFoodChange}
+                  value={selectedFood}
+                >
+                  <option value="">---</option>
+                  {foods.map((food) => (
+                    <option key={food.id} value={food.id}>
+                      {food.type}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </form>
+            <ul className="food_list" >
+              {filteredFood.length > 0 &&
+                filteredFood.map((food) => (
+                  <li key={food.id}>
+                    <Link to={`/foods/${food.id}`}>
+                      <Foods food={food} />
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          </>
+        );
+      }
+
 
 export default FoodPage;
